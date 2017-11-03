b60e0267c833e6431c0b8b5a3f07b8b5a973f1d1
Gustavo Hoirisch <gustavo.hoirisch@myob.com>
2017-11-03 13:46:21 +1100

    add sad path to list

--- pkg/dognzb/dognzb_test.go
+++ pkg/dognzb/dognzb_test.go
@@ -76,6 +76,57 @@ func TestListHappyPath(t *testing.T) {
 	}
 }
 
+func TestListSadPath(t *testing.T) {
+	testCases := []struct {
+		desc   string
+		api    string
+		size   int
+		status int
+		errMsg string
+	}{
+		{
+			desc:   "404_response",
+			api:    "some-api",
+			status: http.StatusNotFound,
+			errMsg: "bad response: 404",
+		}, {
+			desc:   "xml_error_code",
+			api:    "a-valid-api",
+			status: http.StatusOK,
+			errMsg: "Incorrect user credentials",
+		}, {
+			desc:   "empty_response",
+			api:    "a-valid-api",
+			status: http.StatusOK,
+			errMsg: "EOF",
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			// arrange
+			body, _ := ioutil.ReadFile(fmt.Sprintf("./fixtures/list_%s.xml", tC.desc))
+			d := dognzb.New(tC.api, &mockGetter{
+				response: &http.Response{
+					StatusCode: tC.status,
+					Body:       ioutil.NopCloser(bytes.NewReader(body)),
+				},
+			})
+
+			// act
+			_, err := d.List(dognzb.Movies)
+
+			// assert
+			if err == nil {
+				t.Errorf("expected err to be '%v', got '%v'", nil, err)
+			}
+
+			if err.Error() != tC.errMsg {
+				t.Errorf("expected error message to be '%v', got '%v'", tC.errMsg, err.Error())
+			}
+		})
+	}
+}
+
 type mockGetter struct {
 	response *http.Response
 }
